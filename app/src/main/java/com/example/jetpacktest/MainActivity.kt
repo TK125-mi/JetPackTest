6cc4f61e37487405d93136d2cc2f4a1f01f2bc39
tk mi <nothing>
2022-01-09 15:05:47 +0900

    関数をプレビュー

--- app/src/main/java/com/example/jetpacktest/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktest/MainActivity.kt
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            MessageCard("Android")
         }
     }
+}
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
 }
