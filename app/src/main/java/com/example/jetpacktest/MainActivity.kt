3449126c74d4c9551be63400134e7658b14498f9
tk mi <nothing>
2022-01-09 15:22:03 +0900

    関数をプレビューを実行

--- app/src/main/java/com/example/jetpacktest/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +15,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
 @Composable
 fun MessageCard(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
 }
