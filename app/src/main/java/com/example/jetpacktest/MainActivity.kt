2774d054b992cd7d870689e46a8e456f87d86485
tk mi <nothing>
2022-01-18 21:06:04 +0900

    テーマの追加

--- app/src/main/java/com/example/jetpacktest/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktest/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.jetpacktest
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,12 +20,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            MessageCard(Message("Android","JetPack Compose"))
+            MaterialTheme {
+                MessageCard(Message("Android", "JetPack Compose"))
+            }
         }
     }
 }
@@ -38,23 +45,39 @@ fun MessageCard(msg: Message) {
                 .size(40.dp)
                 // Clip image to be shaped as a circle
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(  text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                        style = MaterialTheme.typography.subtitle2)
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(text = msg.body,
+                    modifier = Modifier.padding(all=4.dp),
+                    style = MaterialTheme.typography.body2)
+            }
+
         }
     }
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
+    MaterialTheme {
     MessageCard(
         msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-    )}
+    )
+    }
+}
