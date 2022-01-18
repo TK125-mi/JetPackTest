4a9ebd865f84289411cc101a469f1249dae1b6eb
tk mi <nothing>
2022-01-18 21:32:47 +0900

    アニメーション追加

--- app/src/main/java/com/example/jetpacktest/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktest/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,11 +15,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,7 +57,11 @@ fun MessageCard(msg: Message) {
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        Column (modifier = Modifier.clickable { isExpanded = !isExpanded }){
             Text(  text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
                         style = MaterialTheme.typography.subtitle2)
@@ -60,7 +69,10 @@ fun MessageCard(msg: Message) {
             Spacer(modifier = Modifier.height(4.dp))
             Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
                 Text(text = msg.body,
-                    modifier = Modifier.padding(all=4.dp),
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2)
             }
 
